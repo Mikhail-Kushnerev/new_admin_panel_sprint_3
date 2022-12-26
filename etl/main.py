92022474ad464a8e3e6ce374c54811ead5a00ac6
Mikhail-Kushnerev <mikushnerev@yandex.ru>
2022-12-27 00:09:06 +0300

    can connect to ES

--- etl/main.py
+++ etl/main.py
@@ -1,7 +1,11 @@
+from datetime import datetime
+
 from psycopg2 import connect
 from psycopg2.extras import DictCursor
-from models import PostgreSQL
-from qyeries import genres, persons
+from settings import PostgreSQL
+from qyeries import genres, persons, film_work
+from elasticsearch import Elasticsearch, helpers
+from schema import mappings, settings
 
 
 def main():
@@ -15,4 +19,43 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    es = Elasticsearch(hosts='http://localhost:9200/')
+
+    # body = {
+    #     "dynamic": "strict",
+    #     "properties": {
+    #             "imdb_rating": {
+    #                 "type": "keyword"
+    #             },
+    #             "genre": {
+    #                 "type": "keyword"
+    #             }
+    #         }
+    #     }
+    #
+    # es.indices.create(index='csx', mappings=mappings, settings=settings)
+
+    action = [{
+        "_index": "csx",
+        "_source": {
+            "id": '123qwe',
+            "imdb_rating": 1.62,
+            "genre": ['Game-Show', 'Music', 'Reality-TV'],
+            "title": "q",
+            "description": "description",
+            "director": "director",
+            "writers_names": ['Cole S. McKay', 'Fred Olen Ray'],
+            "actors_names": ['Cole S. McKay', 'Fred Olen Ray'],
+            "actors": {
+                "id": "123",
+                "name": "name"
+            },
+            "writers": {
+                "id": "123",
+                "name": "name"
+            }
+        }
+    }]
+
+    helpers.bulk(es, action)
--- etl/qyeries.py
+++ etl/qyeries.py
@@ -13,3 +13,61 @@ LEFT JOIN content.person_film_work pfw ON pfw.person_id = p.id
 WHERE p.updated_at > '%s'
 ORDER BY p.updated_at
 '''
+
+film_work = '''
+SELECT
+    fw.id,
+    fw.rating AS imdb_rating,
+    COALESCE (
+        ARRAY_AGG(
+            DISTINCT g.name
+        ),
+        '{}'
+    ) AS genre,
+    fw.title,
+    fw.description,
+    COALESCE (
+       ARRAY_AGG(
+           DISTINCT (p.full_name)
+       ) FILTER (WHERE pfw.role = 'director'),
+       '{}'
+   ) as director,
+     COALESCE (
+       ARRAY_AGG(
+           DISTINCT (p.full_name)
+       ) FILTER (WHERE pfw.role = 'writer'),
+       '{}'
+   ) as writers_names,
+    COALESCE (
+       ARRAY_AGG(
+           DISTINCT (p.full_name)
+       ) FILTER (WHERE pfw.role = 'actor'),
+       '{}'
+   ) as actors_names,
+    COALESCE (
+       JSON_AGG(
+           DISTINCT JSONB_BUILD_OBJECT(
+               'id', p.id,
+               'name', p.full_name
+           )
+       ) FILTER (WHERE pfw.role = 'actor'),
+       '[]'
+   ) as actors,
+    COALESCE (
+       JSON_AGG(
+           DISTINCT JSONB_BUILD_OBJECT(
+               'id', p.id,
+               'name', p.full_name
+           )
+       ) FILTER (WHERE pfw.role = 'writer'),
+       '[]'
+   ) as writers
+FROM content.film_work fw
+LEFT JOIN content.person_film_work pfw ON pfw.film_work_id = fw.id
+LEFT JOIN content.person p ON p.id = pfw.person_id
+LEFT JOIN content.genre_film_work gfw ON gfw.film_work_id = fw.id
+LEFT JOIN content.genre g ON gfw.genre_id = g.id
+WHERE fw.updated_at >= '%s'
+GROUP BY fw.id
+ORDER BY fw.updated_at;
+'''
