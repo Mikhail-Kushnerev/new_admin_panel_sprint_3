4f80aa909099656a9db48b4813021984c0129aa3
Mikhail-Kushnerev <mikushnerev@yandex.ru>
2023-01-01 17:34:01 +0300

    added logging after finished load psql -> es

--- postgres_to_es/main.py
+++ postgres_to_es/main.py
@@ -1,11 +1,11 @@
-import os
 import logging
+import os
 from time import sleep
 
 from dotenv import load_dotenv
 
 from load_data import Postgres, Elastic
-from utils import connect_to_db, postgres, State, JsonFileStorage
+from utils import connect_to_db, postgres, State, JsonFileStorage, get_logger
 from utils.settings import PostgresConfig
 
 
@@ -27,7 +27,7 @@ def load_datas_from_psql_to_es():
             elastics: Elastic = Elastic(db_base['elastic'])
             for data in postgres_datas():
                 elastics.create_body_documents(data)
-        print('all')
+        logging.info('All Datas were checked')
         sleep(int(os.getenv('TIME_TO_SLEEP')))
 
 
@@ -36,4 +36,5 @@ def main():
 
 
 if __name__ == '__main__':
+    get_logger()
     main()
